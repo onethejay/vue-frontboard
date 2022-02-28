97112e241a14567319c0c5cb34a4bd4cfcc37848
onethejay <jhjang951@gmail.com>
2022-02-28 23:51:26 +0900

    List.vue 줄 정리

--- src/views/board/List.vue
+++ src/views/board/List.vue
@@ -88,19 +88,17 @@ export default {
       this.$axios.get(this.$serverUrl + "/board/list", {
         params: this.requestBody,
         headers: {}
+      }).then((res) => {
+        if (res.data.result_code === "OK") {
+          this.list = res.data.data
+          this.paging = res.data.pagination
+          this.no = this.paging.total_list_cnt - ((this.paging.page - 1) * this.paging.page_size)
+        }
+      }).catch((err) => {
+        if (err.message.indexOf('Network Error') > -1) {
+          alert('네트워크가 원활하지 않습니다.\n잠시 후 다시 시도해주세요.')
+        }
       })
-          .then((res) => {
-            if (res.data.result_code === "OK") {
-              this.list = res.data.data
-              this.paging = res.data.pagination
-              this.no = this.paging.total_list_cnt - ((this.paging.page - 1) * this.paging.page_size)
-            }
-          })
-          .catch((err) => {
-            if (err.message.indexOf('Network Error') > -1) {
-              alert('네트워크가 원활하지 않습니다.\n잠시 후 다시 시도해주세요.')
-            }
-          })
     },
     fnView(idx) {
       this.requestBody.idx = idx
