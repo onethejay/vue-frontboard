ecf850b05ff712e8a14e51857f6b83d4b7a1fc98
onethejay <jhjang951@gmail.com>
2022-06-20 23:44:34 +0900

    loginAPI init

---
+++ src/service/loginAPI.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+
+const getUserInfo = (userId, userPw) => {
+  const reqData = {
+    'user_id': userId,
+    'user_pw': userPw
+  }
+
+  let serverUrl = '//localhost:8081'
+
+  return axios.post(serverUrl + '/user/login', reqData, {
+    headers: {
+      'Content-type': 'application/json'
+    }
+  })
+}
+
+export default {
+  async doLogin(userId, userPw) {
+    try {
+      const getUserInfoPromise = await getUserInfo(userId, userPw)
+      const [userInfoResponse] = await Promise.all([getUserInfoPromise])
+      if (userInfoResponse.data.length === 0) {
+        return 'notFound'
+      } else {
+        localStorage.setItem('user_token', userInfoResponse.data.user_token)
+        localStorage.setItem('user_role', userInfoResponse.data.user_role)
+        return userInfoResponse
+      }
+    } catch (err) {
+      console.error(err)
+    }
+  }
+}
