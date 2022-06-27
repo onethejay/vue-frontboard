df47832024495cc92800184a60f0e99e73412727
onethejay <jhjang951@gmail.com>
2022-06-27 23:17:39 +0900

    loginAPI 수정

--- src/service/loginAPI.js
+++ src/service/loginAPI.js
@@ -1,17 +1,18 @@
+import axios from 'axios'
+
 const getUserInfo = (userId, userPw) => {
   const reqData = {
     'user_id': userId,
     'user_pw': userPw
   }
 
-  return {
-    'data': {
-      'user_id': reqData.user_id,
-      'user_token': 'user_test_token',
-      'user_role': 'ADM'
-    }
-  }
+  let serverUrl = '//localhost:8081'
 
+  return axios.post(serverUrl + '/user/login', reqData, {
+    headers: {
+      'Content-type': 'application/json'
+    }
+  })
 }
 
 export default {
