8a4d5127793a7f8112ffd5470d82a13937fe3efb
onethejay <jhjang951@gmail.com>
2022-07-04 23:29:01 +0900

    getters 수정

--- src/vuex/getters.js
+++ src/vuex/getters.js
@@ -2,5 +2,4 @@ export default {
     getUserId: state => state.userId,
     getErrorState: state => state.errorState,
     getIsAuth: state => state.isAuth,
-    getLoadingStatus: state => state.loadingStatus,
 }
